191a3931e183d758867254683cbb3989de8a1693
Youssef Fathy <32334265+0xzer0x@users.noreply.github.com>
2024-04-29 14:41:51 +0300

    feat: add inital item update form

--- src/main/java/io/fursan/inventorymanagement/controller/ItemController.java
+++ src/main/java/io/fursan/inventorymanagement/controller/ItemController.java
@@ -4,10 +4,12 @@ import io.fursan.inventorymanagement.dto.ItemDto;
 import io.fursan.inventorymanagement.mapper.impl.ItemMapper;
 import io.fursan.inventorymanagement.service.ItemService;
 import java.util.List;
+import java.util.Optional;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/items")
@@ -25,5 +27,15 @@ public class ItemController {
     List<ItemDto> itemDtos = itemService.findAll().stream().map(itemMapper::mapTo).toList();
     model.addAttribute("items", itemDtos);
     return "items/list-items";
+  }
+
+  @GetMapping("/save")
+  public String saveItem(@RequestParam(required = false) Optional<Integer> id, Model model) {
+    model.addAttribute(
+        "item",
+        id.map(itemId -> itemService.findById(itemId).orElse(null))
+            .map(itemMapper::mapTo)
+            .orElse(new ItemDto()));
+    return "items/save-item";
   }
 }
