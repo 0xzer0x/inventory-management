df345f4aae8f5cb2eded87d1ba9ba21d970238f8
Youssef Fathy <32334265+0xzer0x@users.noreply.github.com>
2024-05-01 21:17:13 +0300

    fix: supplier and item dtos infinite loop stack overflow error

--- src/main/java/io/fursan/inventorymanagement/dto/ItemDto.java
+++ src/main/java/io/fursan/inventorymanagement/dto/ItemDto.java
@@ -1,15 +1,16 @@
 package io.fursan.inventorymanagement.dto;
 
-import io.fursan.inventorymanagement.entity.Supplier;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-@Data
+@Setter
+@Getter
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
@@ -25,5 +26,26 @@ public class ItemDto {
   @Min(value = 0, message = "Price must be positive")
   private Double unitPrice;
 
-  private List<Supplier> suppliers;
+  private List<SupplierDto> suppliers;
+
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj) return true;
+    if (obj == null) return false;
+    if (getClass() != obj.getClass()) return false;
+    return this.id == ((ItemDto) obj).getId();
+  }
+
+  @Override
+  public String toString() {
+    return "ItemDto [id="
+        + id
+        + ", name="
+        + name
+        + ", quantity="
+        + quantity
+        + ", unitPrice="
+        + unitPrice
+        + "]";
+  }
 }
